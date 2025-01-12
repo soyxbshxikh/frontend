0262298b94c9272e76218ef4f50494ca7dab1177
soyxbshxikh <you@example.com>
2025-01-12 17:56:56 +0530

    Half Header Complete

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import Wrapper from "./Wrapper";
+import Link from "next/link";
+import Menu from "./Menu";
 const Header = () => {
-    return (
-        <div className="text">Header</div>
-    )
-}
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState("translate-y-0");
+  const [lastScrollY, setLastScrollY] = useState(0);
 
-export default Header
+  return (
+    <header
+      className={
+        "w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show} "
+      }
+    >
+      <Wrapper>
+        <Link href="/">
+          <img src="/logo.svg" className="w-[40px] md:w-[60px]" />
+        </Link>
+        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} />
+      </Wrapper>
+    </header>
+  );
+};
+
+export default Header;
