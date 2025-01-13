c1248de1a9cf422ab6d001bcc903f67c397cdea1
soyxbshxikh <you@example.com>
2025-01-14 02:09:19 +0530

    Header Complete

--- components/Wrapper.jsx
+++ components/Wrapper.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 function Wrapper({ children, className }) {
   return (
-    <div className='w-full max-w-[1280px] px-5 md:px-10 mx-auto ${className || ""} '>
+    <div
+      className={`w-full max-w-[1280px] px-5 md:px-10 mx-auto ${
+        className || ""
+      }`}
+    >
       {children}
     </div>
   );
