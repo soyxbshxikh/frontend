a95439b9b79b2d352820162c6178f83f1e03bc9d
soyxbshxikh <you@example.com>
2025-01-17 12:38:03 +0530

    yes

--- components/ProductCard.jsx
+++ components/ProductCard.jsx
@@ -3,7 +3,10 @@ import React from "react";
 
 const ProductCard = () => {
   return (
-    <Link href="/product/1" className=" transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer">
+    <Link
+      href="/product/1"
+      className=" transform overflow-hidden bg-white duration-200 hover:scale-105 cursor-pointer"
+    >
       <img className="w-full" src="/product-1.webp" alt="Product Image" />
       <div className="p-4 text-black/[0.9]">
         <h2 className="text-lg font-medium">Product Name</h2>
